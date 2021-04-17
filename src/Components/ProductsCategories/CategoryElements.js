9fa95144ffea15742d2979ecbed776fc4e1ee54c
webImanA <imanaazibou@hotmail.com>
2021-04-17 19:58:47 +0200

    product categories array

---
+++ src/Components/ProductsCategories/CategoryElements.js
@@ -0,0 +1,17 @@
+export const categoryElements = [
+    {
+        title: "Tops",
+        imgUrl: "https://st.mngbcn.com/rcs/pics/static/T6/fotos/S20/67059202_05.jpg?ts=1585758829852&imwidth=412&imdensity=2",
+        url: 'https://www.google.es/'
+    },
+    {
+        title: "Sets",
+        imgUrl: "https://st.mngbcn.com/rcs/pics/static/T6/fotos/S20/67059202_05.jpg?ts=1585758829852&imwidth=412&imdensity=2",
+        url: '#'
+    },
+    {
+        title: "Bags",
+        imgUrl: "https://st.mngbcn.com/rcs/pics/static/T6/fotos/S20/67059202_05.jpg?ts=1585758829852&imwidth=412&imdensity=2",
+        url: '#'
+    }
+]
