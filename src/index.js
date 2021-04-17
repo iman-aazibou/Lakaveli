3d35b71ba18d4eb29dda9e911ae1a26be66776f1
webImanA <imanaazibou@hotmail.com>
2021-04-17 17:56:15 +0200

    Imported Nav component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Nav from './Components/NavComponent/Nav';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Nav />
   </React.StrictMode>,
   document.getElementById('root')
 );
