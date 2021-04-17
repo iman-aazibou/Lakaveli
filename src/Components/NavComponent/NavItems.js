87561fd6573cda29bb9f0c10773f27a3240773a6
webImanA <imanaazibou@hotmail.com>
2021-04-17 17:55:53 +0200

    Nav Items

---
+++ src/Components/NavComponent/NavItems.js
@@ -0,0 +1,14 @@
+export const navItems = [
+    { title: "Home",
+    url: "https://www.google.es/",
+    cName: "title"
+    },
+    { title: "Products",
+    url: "#",
+    cName: "title"
+    },
+    { title: "About",
+    url: "#",
+    cName: "title"
+    },
+]
